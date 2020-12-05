3925e3db26e31055f97dcb0735ddf0df56e0b62c
Lindsey Hoffman <lksh0812@gmail.com>
2020-12-05 10:18:25 -0500

    Update table_schema.sql

--- table_schema.sql
+++ table_schema.sql
@@ -15,6 +15,58 @@ CREATE TABLE primary_2016_ga (
 	total_votes int
 );
 
+CREATE TABLE race_data_2016 (
+	ga_county varchar Primary Key,
+ 	black_male int,
+ 	black_female int,
+ 	black_unknown int,
+ 	white_male int,
+ 	white_female int,
+	white_unknown int,
+ 	asia_pi_male int,
+ 	asia_pi_female int,
+ 	asia_pi_unknown int,
+ 	hisp_lat_male int,
+ 	hisp_lat_female int,
+ 	hisp_lat_unknown int,
+ 	native_am_male int,
+ 	native_am_female int,
+ 	native_am_unknown int,
+ 	other_male int,
+ 	other_female int,
+ 	other_unknown int,
+ 	unknown_male int,
+ 	unknown_female int,
+ 	unknown int,
+ 	total_voters int
+);
+
+CREATE TABLE race_data_2020 (
+	ga_county varchar Primary Key,
+ 	black_male int,
+ 	black_female int,
+ 	black_unknown int,
+ 	white_male int,
+ 	white_female int,
+	white_unknown int,
+ 	asia_pi_male int,
+ 	asia_pi_female int,
+ 	asia_pi_unknown int,
+ 	hisp_lat_male int,
+ 	hisp_lat_female int,
+ 	hisp_lat_unknown int,
+ 	native_am_male int,
+ 	native_am_female int,
+ 	native_am_unknown int,
+ 	other_male int,
+ 	other_female int,
+ 	other_unknown int,
+ 	unknown_male int,
+ 	unknown_female int,
+ 	unknown int,
+ 	total_voters int
+);
+
 ---Check that data loaded
 select * from primary_2016_ga
 select * from election_2020_ga
